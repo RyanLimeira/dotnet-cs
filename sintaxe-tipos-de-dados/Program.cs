1a11f9fa339a9f2142552204ab567d516ed1ca2f
RyanLimeira <limeira.ryan1110@gmail.com>
2023-09-29 17:27:20 -0300

    Estudos - Sintaxe C#

---
+++ sintaxe-tipos-de-dados/Program.cs
@@ -0,0 +1,22 @@
+﻿using bootcamp_dotnet_cs.Models;
+
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual);
+
+// string apresentacao = "Olá, seja bem vindo!";
+
+// int quantidade = 1;
+// quantidade = 10;
+
+// double altura = 1.80;
+
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+
+// Pessoa pessoa1 = new Pessoa();
+
+// pessoa1.Nome = "Ryan";
+// pessoa1.Idade = 21;
+// pessoa1.Apresentar();
